e26b9e67d43717b81790f68e6bc687d27ca98a1c
i-like-robots <matt@maketea.co.uk>
2014-05-28 15:55:57 +0100

    Move ad size mappings into separate config

--- app/assets/javascripts/lib/core/ad_manager_v2.js
+++ app/assets/javascripts/lib/core/ad_manager_v2.js
@@ -1,48 +1,11 @@
-define([ "jquery", "lib/core/ad_unit" ], function($, AdUnit) {
+define([ "jquery", "lib/core/ad_sizes", "lib/core/ad_unit" ], function($, adSizes, AdUnit) {
 
   "use strict";
 
   var defaultConfig = {
     adunits: ".adunit",
     listener: "#js-row--content",
-
-    /* jshint ignore:start */
-    sizeMapping: {
-      billboard: [
-        { browser: [ 980, 0 ], ad_sizes: [ [ 970, 250 ], [ 728, 90 ] ] },
-        { browser: [ 728, 0 ], ad_sizes: [ 728, 90 ] },
-        { browser: [ 0, 0 ], ad_sizes: [ 320, 50 ] }
-      ],
-      leaderboard: [
-        { browser: [ 980, 0 ], ad_sizes: [ [ 970, 66 ], [ 728, 90 ] ] },
-        { browser: [ 728, 0 ], ad_sizes: [ 728, 90 ] },
-        { browser: [ 0, 0 ], ad_sizes: [ 320, 50 ] }
-      ],
-      "leaderboard-content": [
-        { browser: [ 728, 0 ], ad_sizes: [ 728, 90 ] },
-        { browser: [ 0, 0 ], ad_sizes: [ 320, 50 ] }
-      ],
-      mpu: [
-        { browser: [ 0, 0 ], ad_sizes: [ 300, 250 ] }
-      ],
-      "mpu-double": [
-        { browser: [ 728, 0 ], ad_sizes: [ [ 300, 600 ], [ 300, 250 ] ] },
-        { browser: [ 0, 0 ], ad_sizes: [ 300, 250 ] }
-      ],
-      "sponsor-tile": [
-        { browser: [ 0, 0 ], ad_sizes: [ 276,  32 ] }
-      ],
-      "sponsor-logo": [
-        { browser: [ 0, 0 ], ad_sizes: [ 150, 120 ] }
-      ],
-      "traffic-driver": [
-        { browser: [ 0, 0 ], ad_sizes: [ 192, 380 ] }
-      ],
-      background: [
-        { browser: [ 980, 0 ], ad_sizes: [ 1, 1 ] }
-      ]
-    },
-    /* jshint ignore:end */
+    sizeMapping: adSizes,
 
     // Ad targeting properties
     layers: [ "2009.lonelyplanet" ],
