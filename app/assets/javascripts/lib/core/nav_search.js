164d49331b3a5de290e2e31d68a7c1bf7ab1cb49
Remy Bach <remy@bach.me.uk>
2014-06-05 16:09:04 +0100

    Hotfix for broken relative links in nav search autocomplete

--- app/assets/javascripts/lib/core/nav_search.js
+++ app/assets/javascripts/lib/core/nav_search.js
@@ -23,14 +23,13 @@ define([ "jquery", "autocomplete" ], function($, Autocomplete) {
         resultsWrapper: "<div class='autocomplete'></div>",
         resultsContainer: "<div class='autocomplete__results icon--tapered-arrow-up--after icon--white--after'></div>",
         resultsItemHighlightClass: "autocomplete__results__item--highlight",
-        resultsItem: "<a class='autocomplete__results__item icon--{{type}}--before' href='{{slug}}'>{{name}}</a>",
+        resultsItem: "<a class='autocomplete__results__item icon--{{type}}--before' href='http://www.lonelyplanet.com/{{slug}}'>{{name}}</a>",
         searchTermHighlightClass: "autocomplete__search-term--highlight",
         hiddenClass: "is-hidden"
 
       },
       fetch: function(searchTerm, cb) {
         $.ajax({
-          // url: "//localhost:9000/search.json?q=" + searchTerm,
           url: "//www.lonelyplanet.com/search.json?q=" + searchTerm,
           dataType: "json",
           success: function(data) {
