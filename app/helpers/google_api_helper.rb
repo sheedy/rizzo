2c8753f8d9acf16bd16b738356a049c858c453dc
i-like-robots <matt@maketea.co.uk>
2014-06-19 14:42:20 +0100

    Add helper method for google static map images

---
+++ app/helpers/google_api_helper.rb
@@ -0,0 +1,44 @@
+module GoogleApiHelper
+
+  def google_js_api_uri(js_api_key = 'ABQIAAAADUr8Vd6I7bfZ5k4c27F7KxR5cxXriAJsP5a75Cx4cnHTXGWMNxQxhFddQkNg7EBCllU86qgA_ugglg')
+    javascript_path "http://www.google.com/jsapi?key=#{js_api_key}"
+  end
+
+  def google_maps_url(latitude, longitude)
+    "https://www.google.com/maps/place/#{latitude},#{longitude}/"
+  end
+
+  def google_static_map_src(properties, use_stylers=true)
+    src = "https://maps.googleapis.com/maps/api/staticmap"
+    src+= "?size=#{properties[:width]}x#{properties[:height]}"
+    src+= "&zoom=#{properties[:zoom]}&scale=#{properties[:scale]}"
+
+    if properties[:marker]
+      src+= "&markers=icon:#{CGI.escape(properties[:icon])}|#{properties[:latitude]},#{properties[:longitude]}"
+    else
+      src+= "&center=#{properties[:latitude]},#{properties[:longitude]}"
+    end
+
+    if use_stylers
+      stylers = [
+        "feature:water|element:geometry|color:0xcbdae7",
+        "feature:landscape.man_made|element:geometry.fill|color:0xeff1f3",
+        "feature:road|element:labels.text.stroke|color:0xffffff",
+        "feature:road.arterial|element:geometry.fill|color:0xffffff",
+        "feature:road.arterial|element:geometry.stroke|visibility:off",
+        "feature:road.highway|element:geometry.fill|color:0x16c98d",
+        "feature:road.highway|element:geometry.stroke|visibility:off",
+        "feature:road.local|element:geometry.stroke|visibility:off",
+        "feature:road.local|element:labels|visibility:off",
+        "feature:poi.park|element:geometry.fill|color:0xc8e6aa",
+        "feature:poi.school|element:geometry|color:0xdfdad3",
+        "feature:poi.medical|element:geometry|color:0xfa5e5b"
+      ]
+
+      src+= "&style=#{stylers.join('&style=')}"
+    end
+
+    src
+  end
+
+end
---
+++ spec/helpers/google_api_helper_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe GoogleApiHelper do
+
+  describe "Google JS API URI" do
+
+    it "should return a uri to the goole js_api with the key set" do
+      helper.google_js_api_uri.should eq "http://www.google.com/jsapi?key=ABQIAAAADUr8Vd6I7bfZ5k4c27F7KxR5cxXriAJsP5a75Cx4cnHTXGWMNxQxhFddQkNg7EBCllU86qgA_ugglg"
+    end
+
+    it "should allow you to set a different js_api key" do
+      helper.google_js_api_uri('wombats!').should eq "http://www.google.com/jsapi?key=wombats!"
+    end
+
+  end
+
+  describe "Google Maps URL" do
+
+    let(:latitude) { 51.5073 }
+    let(:longitude) { -0.1277 }
+
+    it "should return a URL based on given coordinates" do
+      helper.google_maps_url(latitude, longitude).should eq "https://www.google.com/maps/place/51.5073,-0.1277/"
+    end
+
+  end
+
+  describe "Static Maps image SRC" do
+
+    let(:properties) do
+      {
+        zoom: 15,
+        scale: 2,
+        width: 320,
+        height: 240,
+        marker: false,
+        latitude: 51.5073,
+        longitude: -0.1277
+      }
+    end
+
+    it "should return static map SRC" do
+      helper.google_static_map_src(properties, false).should eq "https://maps.googleapis.com/maps/api/staticmap?size=320x240&zoom=15&scale=2&center=51.5073,-0.1277"
+    end
+
+  end
+
+end
