abb06531414447704291b0833444064e16d60b95
Jesús Merino Parra <jmmerino@gmail.com>
2014-07-10 11:49:32 +0200

    Code review

--- app/assets/javascripts/lib/page/pushstate.js
+++ app/assets/javascripts/lib/page/pushstate.js
@@ -19,61 +19,77 @@ define([
 
   };
 
+  // -------------------------------------------------------------------------
+  // Mixins
+  // -------------------------------------------------------------------------
+
   withPageState.call(PushState.prototype);
+
+  // -------------------------------------------------------------------------
+  // Public
+  // -------------------------------------------------------------------------
 
   PushState.prototype.navigate = function( state, rootUrl, replaceState ) {
 
-    var url = this._createUrl(state, rootUrl);
-    if (this._supportsHistory() || this._supportsHash()){
-      this._setState( url, replaceState);
+    var url = this._createUrl( state, rootUrl );
+    if ( this._supportsHistory() || this._supportsHash() ) {
+      this._setState( url, replaceState );
     } else {
       this.setUrl( url );
     }
 
   };
 
+  // -------------------------------------------------------------------------
+  // Private
+  // -------------------------------------------------------------------------
+
   PushState.prototype._initHistory = function() {
-    if ( this._supportsHistory() ){
+    if ( this._supportsHistory() ) {
       $( window ).bind( "popstate", function() {
         this._handlePopState();
       }.bind(this));
-    }else if ( this._supportsHash() ){
+    } else if ( this._supportsHash() ) {
       // ie8 and ie9
       this.allowHistoryNav = true;
       // Set up our event listener to listen to hashchange (back/forward)
       $(window).on( "hashchange", this._onHashChange );
       // If there's a hash on page load, fire the _onHashChange function and redirect the user to the correct page.
-      if ( this.getHash() ){
+      if ( this.getHash() ) {
         this._onHashChange();
       }
     }
   };
 
   PushState.prototype._handlePopState = function() {
-    if ( !this.popStateFired ){
+    if ( !this.popStateFired ) {
       this.popStateFired = true;
-      if ( this.getUrl() === this.currentUrl )
-        return;
+      if ( this.getUrl() === this.currentUrl ) return;
     }
 
     this.setUrl( this.getUrl() );
   };
 
   PushState.prototype._supportsHistory = function() {
-    if (this.isHistoryEnabled !== undefined ) return this.isHistoryEnabled;
-    return this.isHistoryEnabled = (window.history && window.history.pushState && window.history.replaceState && !navigator.userAgent.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]|WebApps\/.+CFNetwork)/));
+    if (this.isHistoryEnabled === undefined ) {
+      return this.isHistoryEnabled = (window.history && window.history.pushState && window.history.replaceState && !navigator.userAgent.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]|WebApps\/.+CFNetwork)/));
+    }
+
+    return this.isHistoryEnabled;
   };
 
   PushState.prototype._supportsHash = function() {
-    if ( this.isHashEnabled !== undefined ) return this.isHashEnabled;
-    return this.isHashEnabled = ("onhashchange" in window);
+    if ( this.isHashEnabled === undefined ) {
+      return this.isHashEnabled = ("onhashchange" in window);
+    }
+    return this.isHashEnabled;
   };
 
   // If navigating to a subsection we pass in the new document root
   PushState.prototype._createUrl = function( state, documentRoot ) {
-    var params = (state ? "?" + state : "");
+    var params = ( state ? "?" + state : "" );
 
-    if ( this._supportsHistory() ){
+    if ( this._supportsHistory() ) {
       return documentRoot + params;
     } else {
       return "#!" + documentRoot + params;
@@ -81,7 +97,7 @@ define([
   };
 
   PushState.prototype._replaceUrl = function( url ) {
-    if ( this._supportsHistory() || this._supportsHash() ){
+    if ( this._supportsHistory() || this._supportsHash() ) {
       this._setState( url, true );
     } else {
       this.setUrl( url );
@@ -89,8 +105,8 @@ define([
   };
 
   PushState.prototype._setState = function( url, replaceState ) {
-    if ( this._supportsHistory() ){
-      if ( replaceState ){
+    if ( this._supportsHistory() ) {
+      if ( replaceState ) {
         window.history.replaceState({}, null, url);
       } else {
         window.history.pushState({}, null, url);
@@ -110,9 +126,9 @@ define([
     var hash, url;
 
     // Only cause a refresh if it's back/forward
-    if (this.allowHistoryNav){
+    if (this.allowHistoryNav) {
       hash = this.getHash();
-      if (hash){
+      if (hash) {
         url = hash.substring(2);
       } else {
         url = this.getUrl();
