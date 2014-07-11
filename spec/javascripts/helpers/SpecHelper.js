da0b07b425202301447f3e48254af6f0d17c257d
Remy Bach <remy@bach.me.uk>
2014-07-11 10:05:45 +0100

    Update SpecHelper with path to 'picker' to remove error

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -3,3 +3,8 @@ window.lp.supports = {};
 window.lp.isMobile = false;
 window.lpUserStatusCallback = function() {};
 
+require.config({
+  paths: {
+    "picker": "vendor/assets/javascripts/pickadate/lib/picker"
+  }
+});
